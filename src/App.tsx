cec6028fd1386cb062bec4aae8b6264a8207ced9
Kirill Makhnev <kalorigen@gmail.com>
2023-07-16 19:22:02 +0400

    configured project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <div className='content'>
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
